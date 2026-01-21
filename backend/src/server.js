83f2e0abf3ae309994abd4e566d583186ccaaa2b
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2026-01-21 21:08:28 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,14 @@ import dotenv from 'dotenv'
 import authRouter from './routes/auth.route.js'
 import messageRouter from './routes/message.route.js'
 import path from 'path'
+import {connectDB} from './lib/db.js'
 dotenv.config()
 
 const app = express();
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000
+
+app.use(express.json())
 app.use("/api/auth", authRouter)
 app.use("/api/message", messageRouter)
 
@@ -18,4 +21,6 @@ if(process.env.NODE_ENV === "production"){
         res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
     })
 }
-app.listen(PORT, ()=> console.log("server is running on port:" + PORT) )
+app.listen(PORT, ()=> {console.log("server is running on port:" + PORT)
+    connectDB()
+} )
