a96a778942218c25d56d466f7eaade3ffbf3ca43
mohd-sameer-576 <xmohdsameerx576@gmail.com>
2026-01-23 17:27:24 +0530

    check methoud added

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -10,7 +10,7 @@ export const protectRoute = async (req, res, next) => {
         const decoded = jwt.verify(token, ENV.JWT_SECRET)
         if(!decoded) return res.status(401).json({message:"Unauthorized - Invalid token"})
 
-        const user = await User.findById(decoded.id).select('-password')
+        const user = await User.findById(decoded.userId).select('-password')
         if(!user) return res.status(401).json({message:"Unauthorized - User not found"})
         req.user = user
         next()
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,5 +7,8 @@ router.post('/signup', signup)
 router.post('/login', login)
 router.post('/logout', logout)
 router.put('/update-profile', protectRoute, updateprofile)
+router.get('/check', protectRoute, (req,res) =>{
+    res.status(200).json(req.user)
+})
 
 export default router;
